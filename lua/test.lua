290b430a4e1bd25e34461b0a5216e992e26f2d9b
SkyDivingL <68853795+SkyDivingL@users.noreply.github.com>
2020-08-09 17:56:11 +0200

    Delete test.lua

--- lua/test.lua
+++
@@ -1,8 +0,0 @@
-for k, ply in ipairs(player.GetAll()) do
-    if !ply:IsBot() then
-        ply:SetNWFloat('Legs', 200)
-        ply:SetNWFloat('Arms', 200)
-        ply:Give('ttt_bandage')
-        ApplyStatus(ply)
-    end
-end
